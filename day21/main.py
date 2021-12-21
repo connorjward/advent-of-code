02c2806ad322491dee0a46bc3ffade926148b18e
Connor Ward <cward.github@nospammail.net>
2021-12-21 23:03:52 +0000

    failed attempt at 21

--- day21/main.py
+++ day21/main.py
@@ -1,12 +1,15 @@
+import collections
 from dataclasses import dataclass
 import itertools
 
+import numpy as np
 
-# INPUT = 4, 8  # demo
-INPUT = 8, 5  # actual
+
+INPUT = 4, 8  # demo
+# INPUT = 8, 5  # actual
 
 BOARD_SIZE = 10
-MAX_SCORE = 1000
+MAX_SCORE = 12
 
 
 class DeterministicDice:
@@ -20,43 +23,63 @@ class DeterministicDice:
         return next(self._counter)
 
 
+def generate_dirac_rolls():
+    rolls = collections.Counter()
+    for a, b, c in itertools.product(range(1, 4), range(1, 4), range(1, 4)):
+        rolls[a+b+c] += 1
+    return rolls
+
+
+DIRAC_ROLLS = generate_dirac_rolls()
+
+
 @dataclass
 class Player:
 
     pos: int
     score: int = 0
 
+    def copy(self):
+        return type(self)(self.pos, self.score)
 
-def make_move(player, dice):
-    # breakpoint()
-    move = sum(dice.roll() for _ in range(3))
+
+def update_player(player, move):
     player.pos = (player.pos + move) % BOARD_SIZE
     player.score += player.pos + 1  # account for zero-based indexing
 
 
-def play(player1, player2, dice):
-    while True:
-        make_move(player1, dice)
-        if player1.score >= MAX_SCORE:
-            return player1, player2
+def play(player1, player2, ngames):
+    if player1.score >= MAX_SCORE:
+        return np.array([ngames, 0], dtype=int)
+    if player2.score >= MAX_SCORE:
+        return np.array([0, ngames], dtype=int)
 
-        make_move(player2, dice)
-        if player2.score >= MAX_SCORE:
-            return player2, player1
+    wins = np.zeros((2,), dtype=int)
 
-        # print(f"1: {player1}")
-        # print(f"2: {player2}")
-        # breakpoint()
+    for move, count in DIRAC_ROLLS.items():
+        p1 = player1.copy()
+        p2 = player2.copy()
+
+        update_player(p1, move)
+        wins += play(p1, p2, ngames*count)
+
+    for move, count in DIRAC_ROLLS.items():
+        p1 = player1.copy()
+        p2 = player2.copy()
+
+        update_player(p2, move)
+        wins += play(p1, p2, ngames*count)
+
+    return wins
+
 
 if __name__ == "__main__":
     # subtract due to zero-based indexing
     pos1, pos2 = INPUT
     p1 = Player(pos1-1)
     p2 = Player(pos2-1)
-    dice = DeterministicDice()
 
-    winner, loser = play(p1, p2, dice)
+    breakpoint()
+    wins = play(p1, p2, 1)
 
-    print(f"losing point count: {loser.score}")
-    print(f"dice roll count: {dice.nrolls}")
-    print(f"final result: {loser.score*dice.nrolls}")
+    print(f"final result: {max(wins)}")
