d7088950f347fac56177339f32f2c9351e859721
Connor Ward <c.ward20@imperial.ac.uk>
2022-12-08 22:43:52 +0000

    Day 7 finally done

--- 2022/day07/src/main.rs
+++ 2022/day07/src/main.rs
@@ -1,39 +1,16 @@
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs;
+
+struct Directory {
+    name: String,
+    files: HashMap<String, File>,
+    subdir_names: HashSet<String>,
+}
 
 struct File {
     name: String,
     size: usize,
-}
-
-struct Directory {
-    name: String,
-    subdirs: Vec<Directory>,
-    files: Vec<File>,
-}
-
-impl Directory {
-    fn new(name: String) -> Directory {
-        Directory { name: name, subdirs: Vec::new(), files: Vec::new() }
-    }
-
-    fn add_subdir(&mut self, dirname: String) {
-        let mut subdir = Directory::new(dirname);
-        self.subdirs.push(subdir);
-    }
-
-    fn get_subdir(&self, dirname: &String) -> &mut Directory {
-        for mut subdir in self.subdirs.iter() {
-            if subdir.name.as_str() == dirname.as_str() {
-                return &mut subdir;
-            }
-        }
-        panic!("Directory not found");
-    }
-
-    fn add_file(&mut self, file: File) {
-        self.files.push(file); 
-    }
 }
 
 fn main() {
@@ -53,41 +30,113 @@ fn main() {
 }
 
 fn do_part1(infile: &str) {
-    // first build the tree
-    let mut root = Directory::new(String::from("/"));
-    let mut dirstack: Vec<&mut Directory> = Vec::new();
-    let mut current_dir = &mut root;
-    let contents = fs::read_to_string(infile).unwrap();
-    for line in contents.lines() {
-        if line == "$ cd /" {
-            dirstack.clear();
-            let mut current_dir = &mut root;
-        } else if line == "$ cd .." {
-            let mut current_dir = dirstack.pop().unwrap();
-        } else if line.starts_with("$ cd") {
-            dirstack.push(&mut current_dir);
-            let dirname = line.split(" ").nth(2).unwrap().to_string();
-            let mut current_dir = current_dir.get_subdir(&dirname);
-        } else if line == "$ ls" {
-            continue;
-        } else if line.starts_with("dir") {
-            let dirname = line.split(" ").nth(1).unwrap().to_string();
-            current_dir.add_subdir(dirname);
-        } else {  // must be a file
-            let split_line = line.split(" ");
-            let size = split_line.next().unwrap().parse::<usize>().unwrap();
-            let name = split_line.next().unwrap();
-            let file = File { name: name.to_string(), size: size };
-            current_dir.add_file(file);
+    let mut score = 0;
+    let mut dirs = parse_commands(infile);
+    for dirstack in dirs.keys() {
+        let size = compute_dir_size(&dirs, &dirstack);
+        if size <= 100000 {
+            score += size;
         }
     }
-
-    // now traverse and compute
+    println!("Result: {}", score);
 }
 
 fn do_part2(infile: &str) {
+    let max_space = 70000000 - 30000000;
+    let mut dirs = parse_commands(infile);
+    let rootkey = vec![String::from("/")];
+    let amount_to_delete = compute_dir_size(&dirs, &rootkey) - max_space;
+
+    let mut smallest_valid_dir_to_delete = max_space;
+    for dirstack in dirs.keys() {
+        let dirsize = compute_dir_size(&dirs, &dirstack);
+        if dirsize >= amount_to_delete && dirsize < smallest_valid_dir_to_delete {
+            smallest_valid_dir_to_delete = dirsize;
+        }
+    }
+    println!("Result: {}", smallest_valid_dir_to_delete);
+}
+
+fn parse_commands(infile: &str) -> HashMap<Vec<String>, Directory> {
+    let root = Directory {
+        name: String::from("/"),
+        files: HashMap::new(),
+        subdir_names: HashSet::new(),
+    };
+    // dir name -> Directory
+    let mut dirs: HashMap<Vec<String>, Directory> = HashMap::new();
+    let mut dirstack = vec![root.name.clone()];
+    dirs.insert(dirstack.clone(), root);
     let contents = fs::read_to_string(infile).unwrap();
+
     for line in contents.lines() {
-        println!("{}", line);
+        if line.starts_with("$ cd") {
+            let dirname = line.split(" ").nth(2).unwrap();
+            let dirname = String::from(dirname);
+            if dirname == ".." {
+                dirstack.pop();
+            } else if dirname == "/" {
+                dirstack.drain(1..);
+            } else {
+                // if !dirs.contains_key(&dirname) {
+                //     panic!("{} not found but we want to cd into it", dirname);
+                // } else if !dirs[&current_dirname].subdir_names.contains(&dirname) {
+                //     panic!("{} not registered as a subdim", dirname);
+                // }
+                dirstack.push(dirname.clone());
+            }
+        } else if line.starts_with("$ ls") {
+            continue;
+        } else if line.starts_with("dir") {
+            let subdirname = line.split_once(" ").unwrap().1.to_string();
+            let mut substack = dirstack.clone();
+            substack.push(subdirname.clone());
+            if !dirs.contains_key(&substack) {
+                let subdir = Directory {
+                    name: subdirname.clone(),
+                    files: HashMap::new(),
+                    subdir_names: HashSet::new(),
+                };
+                dirs.insert(substack, subdir);
+                // add to list of subdirs for the current directory
+                dirs.get_mut(&dirstack).unwrap().subdir_names.insert(subdirname.clone());
+            }
+        } else {  // must be a file
+            let (size, name) = line.split_once(" ").unwrap();
+            let name = name.to_string();
+            let size = size.parse::<usize>().unwrap();
+
+            let mut files = &mut dirs.get_mut(&dirstack).unwrap().files;
+            if !files.contains_key(&name) {
+                let file = File { name: name.clone(), size: size };
+                files.insert(name, file);
+            }
+        }
+    }
+    dirs
+}
+
+fn compute_dir_size(dirs: &HashMap<Vec<String>, Directory>, dirstack: &Vec<String>) -> usize {
+    let mut size = 0;
+    for subdir_name in dirs[dirstack].subdir_names.iter() {
+        let mut substack = dirstack.clone();
+        substack.push(subdir_name.clone());
+        size += compute_dir_size(dirs, &substack);
+    }
+    for file in dirs[dirstack].files.values() {
+        size += file.size;
+    }
+    size
+}
+
+fn print_dirs(dirs: &HashMap<Vec<String>, Directory>) {
+    for (name, dir) in dirs.iter() {
+        println!("{}: ({})", name.last().unwrap(), compute_dir_size(dirs, &name));
+        for subdir in dir.subdir_names.iter() {
+            println!("    dir {}", subdir);
+        }
+        for file in dir.files.values() {
+            println!("    {} ({})", file.name, file.size);
+        }
     }
 }
